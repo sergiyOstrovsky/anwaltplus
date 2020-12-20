205cd3c3b18c3732310945358e41c8f87afb0bd2
Sergiy Ostrovsky <sergiy.ostrovsky95@gmail.com>
2020-12-21 01:12:36 +0200

    main

--- components/contact/index.js
+++ components/contact/index.js
@@ -55,14 +55,13 @@ const ContactWithMap = () => (
           </Flex>
         </Box>
       </Section>
-      {/* <StyledLink
+      <StyledLink
         target="_blank"
         width={['100%', '100%', '60%']}
         href="https://www.google.com.ua/maps/place/%D0%B2%D1%83%D0%BB%D0%B8%D1%86%D1%8F+%D0%A1%D0%BB%D0%BE%D0%B2%D0%B0%D1%86%D1%8C%D0%BA%D0%BE%D0%B3%D0%BE,+1,+%D0%9B%D1%8C%D0%B2%D1%96%D0%B2,+%D0%9B%D1%8C%D0%B2%D1%96%D0%B2%D1%81%D1%8C%D0%BA%D0%B0+%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C,+79000/@49.8372031,24.0241812,19z/data=!4m5!3m4!1s0x473add6fdfbbe511:0x36de7713f9418349!8m2!3d49.8372857!4d24.0241635?hl=uk"
       >
-        
-      </StyledLink> */}
-      <Img height="auto" width={['100%', '100%', '60%']} src="/map.png" />
+        <Img height="auto" width="100%" src="/map.PNG" />
+      </StyledLink>
     </Flex>
   </Section>
 );
